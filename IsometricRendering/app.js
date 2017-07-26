595a9ad9b0c8274f021a6979fe7d44811296368b
Unknown <jbrink386@gmail.com>
2017-07-26 20:32:48 +0200

    Initial Render Example

--- IsometricRendering/app.js
+++ IsometricRendering/app.js
@@ -1,84 +1,63 @@
-﻿class Point {
-    x: number;
-    y: number;
-
-    constructor(x: number, y: number) {
+var Point = (function () {
+    function Point(x, y) {
         this.x = x;
         this.y = y;
     }
-}
-
-class Tile {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-
-    constructor(x: number, y: number, width: number, height: number) {        
+    return Point;
+}());
+var Tile = (function () {
+    function Tile(x, y, width, height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
     }
-
-    draw(ctx: CanvasRenderingContext2D): void { 
+    Tile.prototype.draw = function (ctx) {
         var offsetX = 150;
         var offsetY = 150;
         var topPadding = 35;
-
         var x = this.x + offsetX;
         var y = this.y - offsetY;
-
         var p1 = this.getIsometricPoint(x, y);
         var p2 = this.getIsometricPoint(x + this.width, y);
         var p3 = this.getIsometricPoint(x + this.width, y + this.height);
         var p4 = this.getIsometricPoint(x, y + this.height);
-
         ctx.fillStyle = this.getColor();
         ctx.strokeStyle = 'black';
         ctx.beginPath();
         ctx.lineTo(p4.x, p4.y);
         ctx.lineTo(p1.x, p1.y);
         ctx.lineTo(p2.x, p2.y);
-        ctx.lineTo(p3.x, p3.y);                
+        ctx.lineTo(p3.x, p3.y);
         ctx.fill();
         ctx.stroke();
         ctx.closePath();
-    }
-
-    getColor(): string {
+    };
+    Tile.prototype.getColor = function () {
         return 'blue';
-    }
-
-    getIsometricPoint(x: number, y: number): Point {
+    };
+    Tile.prototype.getIsometricPoint = function (x, y) {
         return new Point(x - y, (x + y) / 2);
-    }
-}
-
-class IsometricMap {
-    canvas: HTMLCanvasElement;
-    ctx: CanvasRenderingContext2D;
-    grid: Tile[][];
-    tileSize: number = 75;
-
-    constructor(canvas: HTMLCanvasElement) {
+    };
+    return Tile;
+}());
+var IsometricMap = (function () {
+    function IsometricMap(canvas) {
+        this.tileSize = 75;
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
     }
-
-    public init(): void {
-        this.initGrid();        
+    IsometricMap.prototype.init = function () {
+        this.initGrid();
         this.loop();
-    }
-
-    loop() {
+    };
+    IsometricMap.prototype.loop = function () {
+        var _this = this;
         //this.update();
         this.draw();
-
-        requestAnimationFrame(() => this.loop());
-    }
-
-    initGrid(): void {
+        requestAnimationFrame(function () { return _this.loop(); });
+    };
+    IsometricMap.prototype.initGrid = function () {
         this.grid = [];
         for (var row = 0; row < 4; row++) {
             this.grid[row] = [];
@@ -86,19 +65,19 @@ class IsometricMap {
                 this.grid[row][col] = new Tile(col * this.tileSize, row * this.tileSize, this.tileSize, this.tileSize);
             }
         }
-    }
-
-    draw(): void {
+    };
+    IsometricMap.prototype.draw = function () {
         for (var row = 0; row < 4; row++) {
             for (var col = 0; col < 4; col++) {
                 this.grid[row][col].draw(this.ctx);
             }
         }
-    }
-}
-
-window.onload = () => {
+    };
+    return IsometricMap;
+}());
+window.onload = function () {
     var canvas = document.getElementById('myCanvas');
-    var isometricMap = new IsometricMap(canvas as HTMLCanvasElement);
+    var isometricMap = new IsometricMap(canvas);
     isometricMap.init();
 };
+//# sourceMappingURL=app.js.map
