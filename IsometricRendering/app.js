5f8735d28e54d0f2d931b6bf25d55ebfb33d5554
Unknown <jbrink386@gmail.com>
2017-07-29 15:06:56 +0200

    Refactoring, mouse tile collision

--- IsometricRendering/app.js
+++ IsometricRendering/app.js
@@ -1,50 +1,9 @@
-var Point = (function () {
-    function Point(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-    return Point;
-}());
-var Tile = (function () {
-    function Tile(x, y, width, height) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-    Tile.prototype.draw = function (ctx) {
-        var offsetX = 150;
-        var offsetY = 150;
-        var topPadding = 35;
-        var x = this.x + offsetX;
-        var y = this.y - offsetY;
-        var p1 = this.getIsometricPoint(x, y);
-        var p2 = this.getIsometricPoint(x + this.width, y);
-        var p3 = this.getIsometricPoint(x + this.width, y + this.height);
-        var p4 = this.getIsometricPoint(x, y + this.height);
-        ctx.fillStyle = this.getColor();
-        ctx.strokeStyle = 'black';
-        ctx.beginPath();
-        ctx.lineTo(p4.x, p4.y);
-        ctx.lineTo(p1.x, p1.y);
-        ctx.lineTo(p2.x, p2.y);
-        ctx.lineTo(p3.x, p3.y);
-        ctx.fill();
-        ctx.stroke();
-        ctx.closePath();
-    };
-    Tile.prototype.getColor = function () {
-        return 'blue';
-    };
-    Tile.prototype.getIsometricPoint = function (x, y) {
-        return new Point(x - y, (x + y) / 2);
-    };
-    return Tile;
-}());
 var IsometricMap = (function () {
     function IsometricMap(canvas) {
-        this.tileSize = 75;
-        this.castleLoaded = false;
+        this.tileSize = new Point(50, 50);
+        this.imagesLoaded = 0;
+        this.totalImages = 2;
+        this.mapOffset = new Point(this.tileSize.x * 4, this.tileSize.x * 2);
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
     }
@@ -53,16 +12,23 @@ var IsometricMap = (function () {
         this.castle = new Image(75, 75);
         this.castle.onload = (function () { return _this.imageLoaded(); });
         this.castle.src = 'castle.png';
+        this.tile = new Image(100, 50);
+        this.tile.onload = (function () { return _this.imageLoaded(); });
+        this.tile.src = 'tile.png';
+        this.mousePoint = new Point(0, 0);
+        this.mouseText = [];
         this.initGrid();
         this.loop();
     };
     IsometricMap.prototype.imageLoaded = function () {
-        this.castleLoaded = true;
+        this.imagesLoaded++;
     };
     IsometricMap.prototype.loop = function () {
         var _this = this;
-        //this.update();
-        this.draw();
+        if (this.imagesLoaded == this.totalImages) {
+            this.update();
+            this.draw();
+        }
         requestAnimationFrame(function () { return _this.loop(); });
     };
     IsometricMap.prototype.initGrid = function () {
@@ -70,23 +36,62 @@ var IsometricMap = (function () {
         for (var row = 0; row < 4; row++) {
             this.grid[row] = [];
             for (var col = 0; col < 4; col++) {
-                this.grid[row][col] = new Tile(col * this.tileSize, row * this.tileSize, this.tileSize, this.tileSize);
+                var bounds = new Rectangle((col * this.tileSize.x) + this.mapOffset.x, (row * this.tileSize.y) - this.mapOffset.y, this.tileSize.x, this.tileSize.y);
+                this.grid[row][col] = new Tile(bounds, this.tile);
             }
         }
     };
+    IsometricMap.prototype.update = function () {
+        var screen = new Point(this.mousePoint.x, this.mousePoint.y);
+        var cart = this.getCartesianPoint(screen.x, screen.y);
+        cart.x -= this.mapOffset.x;
+        cart.y += this.mapOffset.y;
+        var col = Math.floor(cart.x / this.tileSize.x);
+        var row = Math.floor(cart.y / this.tileSize.y);
+        if ((col < 0 || col > 3) || (row < 0 || row > 3)) {
+            // mouse out of grid bounds
+            return;
+        }
+        this.grid[row][col].selected = true;
+    };
     IsometricMap.prototype.draw = function () {
+        this.ctx.clearRect(0, 0, 600, 300);
+        this.drawMap();
+        this.drawCastle();
+        this.ctx.fillStyle = 'orange';
+        var textY = 10;
+        for (var i = 0; i < this.mouseText.length; i++) {
+            this.ctx.fillText(this.mouseText[i], 200, textY);
+            textY += 15;
+        }
+        for (var i = 0; i < this.mouseText.length; i++) {
+            this.mouseText.pop();
+            i--;
+        }
+    };
+    IsometricMap.prototype.drawMap = function () {
         for (var row = 0; row < 4; row++) {
             for (var col = 0; col < 4; col++) {
                 this.grid[row][col].draw(this.ctx);
             }
         }
-        if (this.castleLoaded) {
-            var tile = this.grid[1][1];
-            var x = tile.x + 150;
-            var y = tile.y - 150;
-            var isoPoint = tile.getIsometricPoint(x, y);
-            this.ctx.drawImage(this.castle, isoPoint.x - (75 / 2), isoPoint.y - (75 / 2), tile.width, tile.height);
-        }
+    };
+    IsometricMap.prototype.drawCastle = function () {
+        var tile = this.grid[1][1];
+        var x = tile.bounds.x;
+        var y = tile.bounds.y;
+        var isoPoint = this.getIsometricPoint(x, y);
+        this.ctx.drawImage(this.castle, isoPoint.x - (this.tileSize.x / 2), isoPoint.y - (this.tileSize.y / 2), tile.bounds.width, tile.bounds.height);
+    };
+    IsometricMap.prototype.mouseMove = function (mousePosition) {
+        this.mousePoint.x = mousePosition.x - this.canvas.offsetLeft;
+        this.mousePoint.y = mousePosition.y - this.canvas.offsetTop;
+    };
+    IsometricMap.prototype.getIsometricPoint = function (x, y) {
+        return new Point(x - y, (x + y) / 2);
+    };
+    IsometricMap.prototype.getCartesianPoint = function (x, y) {
+        return new Point((2 * y + x) / 2, (2 * y - x) / 2);
     };
     return IsometricMap;
 }());
@@ -94,5 +99,6 @@ window.onload = function () {
     var canvas = document.getElementById('myCanvas');
     var isometricMap = new IsometricMap(canvas);
     isometricMap.init();
+    window.addEventListener('mousemove', function (e) { isometricMap.mouseMove(new Point(e.clientX, e.clientY)); });
 };
 //# sourceMappingURL=app.js.map
--- IsometricRendering/app.ts
+++ IsometricRendering/app.ts
@@ -1,67 +1,15 @@
-﻿class Point {
-    x: number;
-    y: number;
-
-    constructor(x: number, y: number) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Tile {
-    x: number;
-    y: number;
-    width: number;
-    height: number;
-
-    constructor(x: number, y: number, width: number, height: number) {        
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
-
-    draw(ctx: CanvasRenderingContext2D): void { 
-        var offsetX = 150;
-        var offsetY = 150;
-        var topPadding = 35;
-
-        var x = this.x + offsetX;
-        var y = this.y - offsetY;
-
-        var p1 = this.getIsometricPoint(x, y);
-        var p2 = this.getIsometricPoint(x + this.width, y);
-        var p3 = this.getIsometricPoint(x + this.width, y + this.height);
-        var p4 = this.getIsometricPoint(x, y + this.height);
-
-        ctx.fillStyle = this.getColor();
-        ctx.strokeStyle = 'black';
-        ctx.beginPath();
-        ctx.lineTo(p4.x, p4.y);
-        ctx.lineTo(p1.x, p1.y);
-        ctx.lineTo(p2.x, p2.y);
-        ctx.lineTo(p3.x, p3.y);                
-        ctx.fill();
-        ctx.stroke();
-        ctx.closePath();
-    }
-
-    getColor(): string {
-        return 'blue';
-    }
-
-    getIsometricPoint(x: number, y: number): Point {
-        return new Point(x - y, (x + y) / 2);
-    }
-}
-
-class IsometricMap {
+﻿class IsometricMap {
     canvas: HTMLCanvasElement;
     ctx: CanvasRenderingContext2D;
     grid: Tile[][];
-    tileSize: number = 75;
-    castleLoaded: boolean = false;
+    tileSize: Point = new Point(50, 50);
+    imagesLoaded: number = 0;
+    totalImages: number = 2;
     castle: HTMLImageElement;
+    tile: HTMLImageElement;
+    mousePoint: Point;
+    mouseText: string[];
+    mapOffset: Point = new Point(this.tileSize.x * 4, this.tileSize.x * 2);
 
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
@@ -70,21 +18,29 @@ class IsometricMap {
 
     public init(): void {
         this.castle = new Image(75, 75);
-        this.castle.onload = (() => this.imageLoaded());  
+        this.castle.onload = (() => this.imageLoaded());
         this.castle.src = 'castle.png';
 
-        this.initGrid();        
+        this.tile = new Image(100, 50);
+        this.tile.onload = (() => this.imageLoaded());
+        this.tile.src = 'tile.png';
+
+        this.mousePoint = new Point(0, 0);
+        this.mouseText = [];
+
+        this.initGrid();
         this.loop();
     }
 
     imageLoaded() {
-        this.castleLoaded = true;
-    }    
+        this.imagesLoaded++;
+    }
 
     loop() {
-        //this.update();
-        this.draw();
-
+        if (this.imagesLoaded == this.totalImages) {
+            this.update();
+            this.draw();
+        }
         requestAnimationFrame(() => this.loop());
     }
 
@@ -93,27 +49,83 @@ class IsometricMap {
         for (var row = 0; row < 4; row++) {
             this.grid[row] = [];
             for (var col = 0; col < 4; col++) {
-                this.grid[row][col] = new Tile(col * this.tileSize, row * this.tileSize, this.tileSize, this.tileSize);
+                var bounds = new Rectangle(
+                    (col * this.tileSize.x) + this.mapOffset.x,
+                    (row * this.tileSize.y) - this.mapOffset.y,
+                    this.tileSize.x,
+                    this.tileSize.y);
+                this.grid[row][col] = new Tile(bounds, this.tile);
             }
         }
     }
 
+    update(): void {
+        var screen = new Point(this.mousePoint.x, this.mousePoint.y);        
+        var cart = this.getCartesianPoint(screen.x, screen.y);  
+        cart.x -= this.mapOffset.x;
+        cart.y += this.mapOffset.y;
+        var col = Math.floor(cart.x / this.tileSize.x);
+        var row = Math.floor(cart.y / this.tileSize.y);
+
+        if ((col < 0 || col > 3) || (row < 0 || row > 3)) {
+            // mouse out of grid bounds
+            return;
+        }
+
+        this.grid[row][col].selected = true;
+    }
+
     draw(): void {
+        this.ctx.clearRect(0, 0, 600, 300);
+
+        this.drawMap();
+        this.drawCastle();
+
+        this.ctx.fillStyle = 'orange';
+        var textY = 10;
+        for (var i = 0; i < this.mouseText.length; i++) {
+            this.ctx.fillText(this.mouseText[i], 200, textY);
+            textY += 15;
+        }  
+
+        for (var i = 0; i < this.mouseText.length; i++) {
+            this.mouseText.pop()
+            i--;
+        }
+    }
+
+    drawMap(): void {
         for (var row = 0; row < 4; row++) {
             for (var col = 0; col < 4; col++) {
                 this.grid[row][col].draw(this.ctx);
             }
         }
+    }
 
-        if (this.castleLoaded) {
-            var tile = this.grid[1][1];
-            var x = tile.x + 150;
-            var y = tile.y - 150;
+    drawCastle(): void {
+        var tile = this.grid[1][1];
+        var x = tile.bounds.x;
+        var y = tile.bounds.y;
+        var isoPoint = this.getIsometricPoint(x, y);
+        this.ctx.drawImage(
+            this.castle,
+            isoPoint.x - (this.tileSize.x / 2),
+            isoPoint.y - (this.tileSize.y / 2),
+            tile.bounds.width,
+            tile.bounds.height);
+    }
 
-            var isoPoint = tile.getIsometricPoint(x, y);
-            this.ctx.drawImage(
-                this.castle, isoPoint.x - (75 / 2), isoPoint.y - (75 / 2), tile.width, tile.height);
-        }
+    mouseMove(mousePosition: Point): void {
+        this.mousePoint.x = mousePosition.x - this.canvas.offsetLeft;
+        this.mousePoint.y = mousePosition.y - this.canvas.offsetTop;
+    }
+
+    getIsometricPoint(x: number, y: number): Point {
+        return new Point(x - y, (x + y) / 2);
+    }
+
+    getCartesianPoint(x: number, y: number): Point {        
+        return new Point((2 * y + x) / 2, (2 * y - x) / 2)
     }
 }
 
@@ -121,4 +133,5 @@ window.onload = () => {
     var canvas = document.getElementById('myCanvas');
     var isometricMap = new IsometricMap(canvas as HTMLCanvasElement);
     isometricMap.init();
+    window.addEventListener('mousemove', function (e) { isometricMap.mouseMove(new Point(e.clientX, e.clientY)); });
 };
