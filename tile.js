b7bb045b4e2e427ccd78068714597b2f3fabbde3
Unknown <jbrink386@gmail.com>
2018-01-30 19:59:52 +0200

    transpile ts to js plus tsConfig

--- tile.js
+++ tile.js
@@ -9,9 +9,6 @@ var Tile = (function () {
         if (!this.selected) {
             ctx.drawImage(this.tile, iso.x - this.bounds.width, iso.y, this.bounds.width * 2, this.bounds.height);
         }
-        //ctx.strokeStyle = 'red';
-        //ctx.beginPath();        
-        //ctx.strokeRect(this.bounds.x, this.bounds.y, this.bounds.width, this.bounds.height);
         this.selected = false;
     };
     Tile.prototype.getColor = function () {
